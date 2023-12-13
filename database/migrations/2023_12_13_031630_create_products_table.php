f0651803c22d633d32a99f874956bf4f056d52a6
PaulEscajadillo <paul.escajadillo.lock@gmail.com>
2023-12-12 23:21:29 -0500

    Migration created for products and reviews

--- database/migrations/2023_12_13_031630_create_products_table.php
+++ database/migrations/2023_12_13_031630_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('detail');
+            $table->float('price');
+            $table->integer('stock');
+            $table->float('discount');
             $table->timestamps();
         });
     }
